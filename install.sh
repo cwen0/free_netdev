a08799f22834738f86c31c901b7ddfc17e1b7122
CWen <cwenyin0@gmail.com>
2019-02-11 17:03:12 +0800

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-make && cp -r free_netdev /usr/local/ 
+make && cp -r ../free_netdev /usr/local/ 
 cp auto-free-netdev.sh /usr/local/bin/ && cp freenetdev.service /etc/systemd/system/
 systemctl enable freenetdev.service && systemctl start freenetdev.service
